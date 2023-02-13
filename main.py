639c6681b4984730f254bd1eccf0e18d69a80955
daspartho <parthodas6176@gmail.com>
2023-02-13 14:27:58 +0530

    bert encoder

---
+++ main.py
@@ -0,0 +1,19 @@
+from torch import nn
+import torch
+
+class BertEncoder(nn.Module):
+
+    def __init__(self, n_layer=12, d_model=768, n_head=12):
+        super().__init__()
+        encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=n_head)
+        self.encoder = nn.TransformerEncoder(encoder_layer=encoder_layer, num_layers=n_layer)
+
+    def forward(self, x):
+        return self.encoder(x)
+
+if __name__=="__main__":
+
+    bert_encoder = BertEncoder()
+    src = torch.rand(10, 32, 768)
+    out = bert_encoder(src)
+    print(out.shape)
