7b98e48290b3c136390eed7df278ba16ac4dcca7
daspartho <parthodas6176@gmail.com>
2023-02-14 00:39:37 +0530

    gelu activation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class BertEncoder(nn.Module):
 
     def __init__(self, n_layer=12, d_model=768, n_head=12):
         super().__init__()
-        encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=n_head)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=n_head, activation="gelu")
         self.encoder = nn.TransformerEncoder(encoder_layer=encoder_layer, num_layers=n_layer)
 
     def forward(self, x):
